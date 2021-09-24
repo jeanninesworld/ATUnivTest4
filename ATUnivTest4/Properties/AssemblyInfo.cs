0676e08f47ef797c0a8e59b4a2778b979eba11a3
Jeannine.Kwasnik <jeanninesworld@hotmail.com>
2021-09-24 15:38:42 -0400

    Creating new branch

---
+++ ATUnivTest4/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ATUnivTest4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AgileThought")]
+[assembly: AssemblyProduct("ATUnivTest4")]
+[assembly: AssemblyCopyright("Copyright © AgileThought 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1bed6415-bd7d-4fac-9c44-6d7dec373d3d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
